054fe6ea09ba8ce466106acbc965556c0e35319c
marco76tv <marco76tv@users.noreply.github.com>
2026-03-08 19:57:46 +0000

    Check & fix styling

--- app/Filament/Clusters/Appearance/Pages/Headernav.php
+++ app/Filament/Clusters/Appearance/Pages/Headernav.php
@@ -20,7 +20,6 @@ use Modules\Cms\Filament\Clusters\Appearance;
 use Modules\Tenant\Actions\Config\ResolveTenantConfigValueAction;
 use Modules\Xot\Actions\Filament\Block\GetViewBlocksOptionsByTypeAction;
 use Modules\Xot\Filament\Pages\XotBasePage;
-use Webmozart\Assert\Assert;
 
 /**
  * Page class for managing header navigation appearance settings.
