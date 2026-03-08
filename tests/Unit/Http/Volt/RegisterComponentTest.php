8d46cf283429e9d4dbd78fd283c65c888572ae72
marco76tv <marco76tv@users.noreply.github.com>
2026-03-08 09:42:55 +0000

    Check & fix styling

--- tests/Unit/Http/Volt/RegisterComponentTest.php
+++ tests/Unit/Http/Volt/RegisterComponentTest.php
@@ -6,7 +6,6 @@ namespace Modules\Cms\Tests\Unit\Http\Volt;
 
 use Livewire\Volt\Component as VoltComponent;
 use Modules\Cms\Http\Volt\RegisterComponent;
-use ReflectionClass;
 
 uses(\Modules\Cms\Tests\TestCase::class);
 
@@ -35,7 +34,7 @@ describe('RegisterComponent', function (): void {
     });
 
     test('register method returns redirect response', function (): void {
-        $reflection = new ReflectionClass(RegisterComponent::class);
+        $reflection = new \ReflectionClass(RegisterComponent::class);
         $method = $reflection->getMethod('register');
         $returnType = $method->getReturnType();
 
