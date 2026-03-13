361777ac33a103000c1f50f473a84d2f53a94fce
marco76tv <marco.sottana@gmail.com>
2026-03-13 14:59:29 +0100

    .

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -5,18 +5,16 @@ declare(strict_types=1);
 namespace Modules\Cms\Models;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Carbon;
 use Modules\Cms\Database\Factories\PageFactory;
 use Modules\Cms\Models\Traits\HasBlocks;
-use Modules\Tenant\Contracts\SushiToJsonContract;
-use Modules\Tenant\Models\Traits\SushiToJson;
+use Modules\Tenant\Models\Traits\SushiToJsons;
 use Modules\Xot\Contracts\ProfileContract;
 
 /**
  * Modules\Cms\Models\Page.
- *
- * @method static array<int, array<string, mixed>> getMiddlewareBySlug(string $slug)
- * @method static array<string, \Modules\Cms\Datas\BlockData> getBlocksBySlug(string $slug, ?string $side = null)
  *
  * @property string                       $id
  * @property array<array-key, mixed>|null $title
@@ -34,6 +32,7 @@ use Modules\Xot\Contracts\ProfileContract;
  * @property ProfileContract|null         $creator
  * @property mixed                        $translations
  * @property ProfileContract|null         $updater
+ *
  * @method static Builder<static>|Page    newModelQuery()
  * @method static Builder<static>|Page    newQuery()
  * @method static Builder<static>|Page    query()
@@ -177,23 +176,25 @@ use Modules\Xot\Contracts\ProfileContract;
  * @method static static                  onlyTrashed()
  * @method static static                  withTrashed()
  * @method static static                  withoutTrashed()
- * @method static \Illuminate\Database\Eloquent\Collection<int, static> all($columns = ['*'])
- * @method static \Illuminate\Database\Eloquent\Collection<int, static> get($columns = ['*'])
+ * @method static Collection<int, static> all($columns = ['*'])
+ * @method static Collection<int, static> get($columns = ['*'])
  * @method static static|null             first($columns = ['*'])
  * @method static static|null             find($id, $columns = ['*'])
+ *
  * @property ProfileContract|null $deleter
+ *
  * @method static PageFactory factory($count = null, $state = [])
+ *
  * @property array<array-key, mixed>|null $blocks
+ *
  * @method static Builder<static>|Page whereBlocks($value)
- * @method array<int, array<string, mixed>> getSushiRows()
- * @method static array<string, \Modules\Cms\Datas\BlockData> getBlocksBySlug(string $slug, ?string $side = null)
- * @method static array<int, string> getMiddlewareBySlug(string $slug)
+ *
  * @mixin \Eloquent
  */
-class Page extends BaseModelLang implements SushiToJsonContract
+class Page extends BaseModelLang
 {
     use HasBlocks;
-    use SushiToJson;
+    use SushiToJsons;
 
     /** @var array<int, string> */
     public $translatable = [
@@ -239,9 +240,21 @@ class Page extends BaseModelLang implements SushiToJsonContract
         return $this->getSushiRows();
     }
 
+    public static function findUniqueBySlug(string $slug): ?self
+    {
+        try {
+            /** @var self $page */
+            $page = self::query()->where('slug', $slug)->sole();
+
+            return $page;
+        } catch (ModelNotFoundException) {
+            return null;
+        }
+    }
+
     public static function getMiddlewareBySlug(string $slug): array
     {
-        $page = self::where('slug', $slug)->first();
+        $page = self::findUniqueBySlug($slug);
 
         if (! $page instanceof Page) {
             return [];
