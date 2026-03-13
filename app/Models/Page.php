09d3256f1d16e3eea575de0f09a94ee592bde670
marco76tv <marco76tv@users.noreply.github.com>
2026-03-13 06:55:18 +0000

    Check & fix styling

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Modules\Cms\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
