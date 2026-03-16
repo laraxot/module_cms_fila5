4b1fc76519215883280e19023c5c56db20cb27dd
marco76tv <marco.sottana@gmail.com>
2026-03-16 18:31:09 +0100

    .

--- resources/views/components/page.blade.php
+++ resources/views/components/page.blade.php
@@ -15,8 +15,27 @@ declare(strict_types=1);
 @if(!empty($blocks))
     <div class="page-{{ $side }}-content" data-slug="{{ $slug }}" data-side="{{ $side }}">
         @foreach($blocks as $block)
-            {{-- BlockData ha già gestito tutto: vista, dati, fallback --}}
-            @include($block->view, array_merge($data, $block->data))
+            @if(isset($block->data['component']))
+                @php
+                    $livewireParams = [];
+                    foreach (($block->data['params'] ?? []) as $paramKey => $paramValue) {
+                        $resolvedKey = str_ends_with($paramKey, '_key') ? substr($paramKey, 0, -4) : $paramKey;
+                        if (str_ends_with($paramKey, '_key') && is_string($paramValue)) {
+                            $parts = explode('.', $paramValue);
+                            $val = $data;
+                            foreach ($parts as $part) {
+                                $val = is_array($val) ? ($val[$part] ?? null) : (is_object($val) ? ($val->{$part} ?? null) : null);
+                            }
+                            $livewireParams[$resolvedKey] = $val;
+                        } else {
+                            $livewireParams[$resolvedKey] = $paramValue;
+                        }
+                    }
+                @endphp
+                @livewire($block->data['component'], $livewireParams)
+            @else
+                @include($block->view, array_merge($data, $block->data))
+            @endif
         @endforeach
     </div>
 @endif
