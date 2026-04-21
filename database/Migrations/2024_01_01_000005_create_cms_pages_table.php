06d906e3c2ea319a910ec43bff7f962513785f59
marco76tv <marco.sottana@gmail.com>
2026-04-21 07:55:05 +0200

    .

--- database/Migrations/2024_01_01_000005_create_cms_pages_table.php
+++ database/Migrations/2024_01_01_000005_create_cms_pages_table.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use Illuminate\Database\Query\Expression;
 use Illuminate\Database\Schema\Blueprint;
 use Modules\Cms\Models\Page;
 use Modules\Xot\Database\Migrations\XotBaseMigration;
@@ -19,40 +18,36 @@ return new class extends XotBaseMigration {
     public function up(): void
     {
         // -- CREATE --
-        $this->tableCreate(static function (Blueprint $table))
+        $this->tableCreate(static function (Blueprint $table): void {
             $table->id();
-
             $table->string('slug')->unique()->index();
             $table->string('title');
             $table->text('content')->nullable();
         });
+
         // -- UPDATE --
-        $this->tableUpdate(function (Blueprint $table))
-            if ($hasColumn('content'))
+        $this->tableUpdate(function (Blueprint $table): void {
+            if ($this->hasColumn('content')) {
                 $table->text('content')->nullable()->change();
             }
 
-            if (! $this->hasColumn('content_blocks'))
+            if (! $this->hasColumn('content_blocks')) {
                 $table->json('content_blocks')->nullable();
-
-                // $table->json('content_blocks')->default(new Expression('(JSON_ARRAY())'));
             }
 
-            if (! $this->hasColumn('sidebar_blocks'))
+            if (! $this->hasColumn('sidebar_blocks')) {
                 $table->json('sidebar_blocks')->nullable();
-
-                // $table->json('sidebar_blocks')->default(new Expression('(JSON_ARRAY())'));
             }
-            if (! $this->hasColumn('footer_blocks'))
+
+            if (! $this->hasColumn('footer_blocks')) {
                 $table->json('footer_blocks')->nullable();
-
-                // $table->json('footer_blocks')->default(new Expression('(JSON_ARRAY())'));
             }
-            if (! $this->hasColumn('slug'))
+
+            if (! $this->hasColumn('slug')) {
                 $table->string('slug')->index();
             }
 
-            $this->updateTimestamps()
+            $this->updateTimestamps(
                 table: $table,
                 hasSoftDeletes: true,
             );
