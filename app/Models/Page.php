41d50fd2a36de8c28106c477a56c7273884f16f2
Malebestia <malebestia@yahoo.it>
2026-04-21 18:24:21 +0200

    .

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -31,6 +31,7 @@ use Modules\Xot\Contracts\ProfileContract;
  * @property array<array-key, mixed>|null $middleware
  * @property string|null                  $content
  * @property string|null                  $description
+ * @property array<array-key, mixed>|null $blocks
  * @property array<array-key, mixed>|null $content_blocks
  * @property array<array-key, mixed>|null $sidebar_blocks
  * @property array<array-key, mixed>|null $footer_blocks
@@ -38,10 +39,13 @@ use Modules\Xot\Contracts\ProfileContract;
  * @property Carbon|null                  $updated_at
  * @property string|null                  $created_by
  * @property string|null                  $updated_by
+ * @property string|null                  $deleted_by
  * @property ProfileContract|null         $creator
- * @property mixed                        $translations
  * @property ProfileContract|null         $updater
+ * @property ProfileContract|null         $deleter
+ * @property mixed                        $translations
  *
+<<<<<<< Updated upstream
  * <<<<<<< HEAD
  *
  * @method static Builder<static>|Page                                  newModelQuery()
@@ -354,6 +358,37 @@ use Modules\Xot\Contracts\ProfileContract;
  * @method static array<string, \Modules\Cms\Datas\BlockData> getBlocksBySlug(string $slug, ?string $side = null)
  * @method static array<int, string>                          getMiddlewareBySlug(string $slug)
  *                                                                                                                >>>>>>> 5580e39 (.)
+=======
+ * @method static PageFactory factory($count = null, $state = [])
+ * @method static Builder<static>|Page newModelQuery()
+ * @method static Builder<static>|Page newQuery()
+ * @method static Builder<static>|Page query()
+ * @method static Builder<static>|Page whereBlocks($value)
+ * @method static Builder<static>|Page whereContent($value)
+ * @method static Builder<static>|Page whereContentBlocks($value)
+ * @method static Builder<static>|Page whereCreatedAt($value)
+ * @method static Builder<static>|Page whereCreatedBy($value)
+ * @method static Builder<static>|Page whereDescription($value)
+ * @method static Builder<static>|Page whereFooterBlocks($value)
+ * @method static Builder<static>|Page whereId($value)
+ * @method static Builder<static>|Page whereMiddleware($value)
+ * @method static Builder<static>|Page whereSidebarBlocks($value)
+ * @method static Builder<static>|Page whereSlug($value)
+ * @method static Builder<static>|Page whereTitle($value)
+ * @method static Builder<static>|Page whereUpdatedAt($value)
+ * @method static Builder<static>|Page whereUpdatedBy($value)
+ * @method static Builder<static>|Page whereJsonContainsLocale(string $column, string $locale, ?mixed $value, string $operand = '=')
+ * @method static Builder<static>|Page whereJsonContainsLocales(string $column, array $locales, ?mixed $value, string $operand = '=')
+ * @method static Builder<static>|Page whereLocale(string $column, string $locale)
+ * @method static Builder<static>|Page whereLocales(string $column, array $locales)
+ * @method static Collection<int, static> all($columns = ['*'])
+ * @method static Collection<int, static> get($columns = ['*'])
+ * @method static static|null first($columns = ['*'])
+ * @method static static|null find($id, $columns = ['*'])
+ * @method static array<string, \Modules\Cms\Datas\BlockData> getBlocksBySlug(string $slug, ?string $side = null)
+ * @method static array<int, string> getMiddlewareBySlug(string $slug)
+ * @method array<int, array<string, mixed>> getSushiRows()
+>>>>>>> Stashed changes
  *
  * @mixin \Eloquent
  */
