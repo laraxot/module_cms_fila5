77953dc556124f88371202ae6f9675ea93fee06a
marco76tv <marco.sottana@gmail.com>
2026-04-21 07:55:41 +0200

    .

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\Cms\Models;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
