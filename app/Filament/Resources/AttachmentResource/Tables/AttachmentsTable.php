9040598773d8606097a7ee5759b724123502c6b0
marco76tv <marco.sottana@gmail.com>
2026-05-26 15:53:45 +0200

    .

--- app/Filament/Resources/AttachmentResource/Tables/AttachmentsTable.php
+++ app/Filament/Resources/AttachmentResource/Tables/AttachmentsTable.php
@@ -9,7 +9,7 @@ use Modules\Xot\Filament\Resources\Tables\XotBaseResourceTable;
 
 class AttachmentsTable extends XotBaseResourceTable
 {
-    public static function getTableColumns(): array
+    public function getTableColumns(): array
     {
         return [
             'title' => TextColumn::make('title')->searchable()->sortable(),
