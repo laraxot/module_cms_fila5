7a9213b31c86843dab4d5bab50942e66d925198b
Malebestia <malebestia@yahoo.it>
2026-05-28 18:49:16 +0200

    .

--- app/Enums/AttachmentDiskEnum.php
+++ app/Enums/AttachmentDiskEnum.php
@@ -4,36 +4,24 @@ declare(strict_types=1);
 
 namespace Modules\Cms\Enums;
 
+use Modules\Xot\Traits\EnumTrait;
 use Filament\Support\Contracts\HasColor;
 use Filament\Support\Contracts\HasIcon;
 use Filament\Support\Contracts\HasLabel;
-use Modules\Xot\Filament\Traits\TransTrait;
 
 enum AttachmentDiskEnum: string implements HasColor, HasIcon, HasLabel
 {
-    use TransTrait;
+    use EnumTrait;
 
     case public_html = 'public_html';
     case videos = 'videos';
     case local = 'local';
 
-    public function getLabel(): string
-    {
-        return $this->transClass(self::class, $this->value.'.label');
-    }
+    
 
-    public function getColor(): string
-    {
-        return $this->transClass(self::class, $this->value.'.color');
-    }
+    
 
-    public function getIcon(): string
-    {
-        return $this->transClass(self::class, $this->value.'.icon');
-    }
+    
 
-    public function getDescription(): string
-    {
-        return $this->transClass(self::class, $this->value.'.description');
-    }
+    
 }
