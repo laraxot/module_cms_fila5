acf27269895e38d79561a4672de4baaf275623f1
marco76tv <marco76tv@users.noreply.github.com>
2026-07-01 08:38:22 +0000

    Check & fix styling

--- tests/Feature/Auth/LoginHttpTest.php
+++ tests/Feature/Auth/LoginHttpTest.php
@@ -8,9 +8,10 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Livewire\Volt\Volt as LivewireVolt;
 use Modules\Cms\Tests\TestCase;
-use PHPUnit\Framework\Assert;
 
 use function Pest\Laravel\actingAs;
+
+use PHPUnit\Framework\Assert;
 
 uses(TestCase::class);
 
@@ -127,7 +128,7 @@ describe('Login Http', function (): void {
             'password' => Hash::make('password123'),
         ]);
 
-        for ($i = 0; $i < 5; $i++) {
+        for ($i = 0; $i < 5; ++$i) {
             LivewireVolt::test('auth.login')
                 ->set('email', $email)
                 ->set('password', 'wrong_password')
