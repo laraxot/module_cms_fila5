3fa3f88fde1e870c4af616d589a55403af95c42c
marco76tv <marco76tv@users.noreply.github.com>
2026-07-06 19:56:05 +0000

    Check & fix styling

--- tests/Feature/Frontoffice/FolioRoutes/PagesSlugRouteTest.php
+++ tests/Feature/Frontoffice/FolioRoutes/PagesSlugRouteTest.php
@@ -7,4 +7,5 @@ use Modules\Cms\Tests\TestCase;
 uses(TestCase::class);
 
 /* @phpstan-ignore method.internalClass */
-it('SKIP dynamic /it/{slug}', function (): void {})->skip('Dynamic pages slug requires fixture.');
+it('SKIP dynamic /it/{slug}', function (): void {
+})->skip('Dynamic pages slug requires fixture.');
