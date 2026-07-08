4f41db5c09420734050eb13e31c42ab74c09aee0
marco76tv <marco76tv@users.noreply.github.com>
2026-07-08 21:19:09 +0000

    Check & fix styling

--- app/Models/BaseTreeModel.php
+++ app/Models/BaseTreeModel.php
@@ -18,15 +18,15 @@ use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
 /**
  * Modules\Cms\Models\BaseTreeModel.
  *
- * @property int $id
- * @property string $name
+ * @property int                    $id
+ * @property string                 $name
  * @property array<int, mixed>|null $items
- * @property Carbon|null $created_at
- * @property Carbon|null $updated_at
- * @property string|null $updated_by
- * @property string|null $created_by
- * @property Carbon|null $deleted_at
- * @property string|null $deleted_by
+ * @property Carbon|null            $created_at
+ * @property Carbon|null            $updated_at
+ * @property string|null            $updated_by
+ * @property string|null            $created_by
+ * @property Carbon|null            $deleted_at
+ * @property string|null            $deleted_by
  *
  * @method static Builder|Menu newModelQuery()
  * @method static Builder|Menu newQuery()
@@ -44,38 +44,38 @@ use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
  * @method static Builder|Menu withTrashed()
  * @method static Builder|Menu withoutTrashed()
  *
- * @property string $title
- * @property int|null $parent_id
+ * @property string                                     $title
+ * @property int|null                                   $parent_id
  * @property Collection<int, static>|array<int, static> $children
- * @property int|null $children_count
- * @property MediaCollection<int, Media> $media
- * @property int|null $media_count
- * @property Menu|null $parent
- * @property Collection<int, static>|array<int, static> $ancestors The model's recursive parents.
- * @property int|null $ancestors_count
- * @property Collection<int, static>|array<int, static> $ancestorsAndSelf The model's recursive parents and itself.
- * @property int|null $ancestors_and_self_count
- * @property Collection<int, static>|array<int, static> $bloodline The model's ancestors, descendants and itself.
- * @property int|null $bloodline_count
- * @property Collection<int, static>|array<int, static> $childrenAndSelf The model's direct children and itself.
- * @property int|null $children_and_self_count
- * @property Collection<int, static>|array<int, static> $descendants The model's recursive children.
- * @property int|null $descendants_count
- * @property Collection<int, static>|array<int, static> $descendantsAndSelf The model's recursive children and itself.
- * @property int|null $descendants_and_self_count
- * @property Collection<int, static>|array<int, static> $parentAndSelf The model's direct parent and itself.
- * @property int|null $parent_and_self_count
- * @property Menu|null $rootAncestor The model's topmost parent.
- * @property Collection<int, static>|array<int, static> $siblings The parent's other children.
- * @property int|null $siblings_count
- * @property Collection<int, static>|array<int, static> $siblingsAndSelf All the parent's children.
- * @property int|null $siblings_and_self_count
+ * @property int|null                                   $children_count
+ * @property MediaCollection<int, Media>                $media
+ * @property int|null                                   $media_count
+ * @property Menu|null                                  $parent
+ * @property Collection<int, static>|array<int, static> $ancestors                  The model's recursive parents.
+ * @property int|null                                   $ancestors_count
+ * @property Collection<int, static>|array<int, static> $ancestorsAndSelf           The model's recursive parents and itself.
+ * @property int|null                                   $ancestors_and_self_count
+ * @property Collection<int, static>|array<int, static> $bloodline                  The model's ancestors, descendants and itself.
+ * @property int|null                                   $bloodline_count
+ * @property Collection<int, static>|array<int, static> $childrenAndSelf            The model's direct children and itself.
+ * @property int|null                                   $children_and_self_count
+ * @property Collection<int, static>|array<int, static> $descendants                The model's recursive children.
+ * @property int|null                                   $descendants_count
+ * @property Collection<int, static>|array<int, static> $descendantsAndSelf         The model's recursive children and itself.
+ * @property int|null                                   $descendants_and_self_count
+ * @property Collection<int, static>|array<int, static> $parentAndSelf              The model's direct parent and itself.
+ * @property int|null                                   $parent_and_self_count
+ * @property Menu|null                                  $rootAncestor               The model's topmost parent.
+ * @property Collection<int, static>|array<int, static> $siblings                   The parent's other children.
+ * @property int|null                                   $siblings_count
+ * @property Collection<int, static>|array<int, static> $siblingsAndSelf            All the parent's children.
+ * @property int|null                                   $siblings_and_self_count
  *
- * @method static Collection<int, static> all($columns = ['*'])
+ * @method static Collection<int, static>                                 all($columns = ['*'])
  * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Menu breadthFirst()
  * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Menu depthFirst()
  * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Menu doesntHaveChildren()
- * @method static Collection<int, static> get($columns = ['*'])
+ * @method static Collection<int, static>                                 get($columns = ['*'])
  * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Menu getExpressionGrammar()
  * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Menu hasChildren()
  * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Menu hasParent()
@@ -88,18 +88,18 @@ use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
  * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Menu whereTitle($value)
  * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Menu withGlobalScopes(array<string, mixed> $scopes)
  * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Menu withRelationshipExpression($direction, callable $constraint, $initialDepth, $from = null, $maxDepth = null)
- * @method static Collection<int, static> all($columns = ['*'])
- * @method static Collection<int, static> get($columns = ['*'])
- * @method static Collection<int, static> all($columns = ['*'])
- * @method static Collection<int, static> get($columns = ['*'])
- * @method static Collection<int, static> all($columns = ['*'])
- * @method static Collection<int, static> get($columns = ['*'])
- * @method static Collection<int, static> all($columns = ['*'])
- * @method static Collection<int, static> get($columns = ['*'])
- * @method static Collection<int, static> all($columns = ['*'])
- * @method static Collection<int, static> get($columns = ['*'])
- * @method static Collection<int, static> all($columns = ['*'])
- * @method static Collection<int, static> get($columns = ['*'])
+ * @method static Collection<int, static>                                 all($columns = ['*'])
+ * @method static Collection<int, static>                                 get($columns = ['*'])
+ * @method static Collection<int, static>                                 all($columns = ['*'])
+ * @method static Collection<int, static>                                 get($columns = ['*'])
+ * @method static Collection<int, static>                                 all($columns = ['*'])
+ * @method static Collection<int, static>                                 get($columns = ['*'])
+ * @method static Collection<int, static>                                 all($columns = ['*'])
+ * @method static Collection<int, static>                                 get($columns = ['*'])
+ * @method static Collection<int, static>                                 all($columns = ['*'])
+ * @method static Collection<int, static>                                 get($columns = ['*'])
+ * @method static Collection<int, static>                                 all($columns = ['*'])
+ * @method static Collection<int, static>                                 get($columns = ['*'])
  *
  * @property ProfileContract|null $creator
  * @property ProfileContract|null $updater
@@ -132,7 +132,7 @@ use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
  * @method static Collection<int, static> get($columns = ['*'])
  * @method static Collection<int, static> all($columns = ['*'])
  * @method static Collection<int, static> get($columns = ['*'])
- * @method static MenuFactory factory($count = null, $state = [])
+ * @method static MenuFactory             factory($count = null, $state = [])
  *
  * @mixin \Eloquent
  */
