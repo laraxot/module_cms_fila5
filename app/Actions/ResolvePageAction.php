8f1401ecf43c29442704366effaa8025953eabc5
Marco Xot <marco76tv@hotmail.com>
2026-08-06 18:56:44 +0200

    .

--- app/Actions/ResolvePageAction.php
+++ app/Actions/ResolvePageAction.php
@@ -28,7 +28,7 @@ final class ResolvePageAction
     {
         $item = $this->loadDynamicModel($container0, $slug0);
 
-        if (null !== $item) {
+        if ($item !== null) {
             return new ResolvePageData(
                 renderMode: 'model',
                 item: $item,
@@ -93,7 +93,7 @@ final class ResolvePageAction
 
         foreach ($possibleModels as $modelClass) {
             $item = $this->queryModel($modelClass, $slug0);
-            if (null !== $item) {
+            if ($item !== null) {
                 return $item;
             }
         }
