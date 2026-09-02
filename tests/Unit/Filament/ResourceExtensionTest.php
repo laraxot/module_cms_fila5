d5e1f567a090b11a92cd29cadb9011a2e5b045a2
marco76tv <marco76tv@users.noreply.github.com>
2026-09-02 20:24:21 +0000

    Check & fix styling

--- tests/Unit/Filament/ResourceExtensionTest.php
+++ tests/Unit/Filament/ResourceExtensionTest.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 use Modules\Cms\Tests\TestCase;
 
-uses(\Modules\Cms\Tests\TestCase::class);
+uses(TestCase::class);
 describe('CMS Resource Extension', function () {
     it('resource extension placeholder')->todo();
 });
