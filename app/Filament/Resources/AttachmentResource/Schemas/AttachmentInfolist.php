b4f6b73264b9960bd1ac6178411487a80bd8c3ca
Marco Xot <marco76tv@hotmail.com>
2026-09-07 22:34:25 +0200

    .

--- app/Filament/Resources/AttachmentResource/Schemas/AttachmentInfolist.php
+++ app/Filament/Resources/AttachmentResource/Schemas/AttachmentInfolist.php
@@ -14,7 +14,7 @@ class AttachmentInfolist extends XotBaseResourceInfolist
     /**
      * @return array<int|string, Component|Htmlable|string>
      */
-    public static function getInfolistSchema(): array
+    public function getInfolistSchema(): array
     {
         return [
             'title' => TextEntry::make('title'),
