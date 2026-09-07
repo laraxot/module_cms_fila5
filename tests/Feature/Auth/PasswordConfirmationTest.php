f66db979879f8538194c5b1de44ffc4b3229191a
Marco Xot <marco76tv@hotmail.com>
2026-09-07 08:05:58 +0200

    fix: PHPStan Cms, bmad story, docs/stories, second brain

--- tests/Feature/Auth/PasswordConfirmationTest.php
+++ tests/Feature/Auth/PasswordConfirmationTest.php
@@ -7,6 +7,4 @@ uses(TestCase::class);
 describe('Password Confirmation', function () {
 test('password confirmation placeholder', function () {
     })->todo('Serve il pannello con le rotte di conferma password: il test va scritto sul flusso, non sul segnaposto.');
-    test('password confirmation placeholder', function () {
-    })->todo('Serve il pannello con le rotte di conferma password: il test va scritto sul flusso, non sul segnaposto.');
 });
