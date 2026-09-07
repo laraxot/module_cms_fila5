643b77b92450a3ae0d5e7fc8c3be8a6a4b20adfd
Marco Xot <marco76tv@hotmail.com>
2026-09-07 22:34:25 +0200

    .

--- app/Filament/Resources/MenuResource/Schemas/MenuInfolist.php
+++ app/Filament/Resources/MenuResource/Schemas/MenuInfolist.php
@@ -13,7 +13,7 @@ class MenuInfolist extends XotBaseResourceInfolist
     /**
      * @return array<string, Component>
      */
-    public static function getInfolistSchema(): array
+    public function getInfolistSchema(): array
     {
         return [
             'title' => TextEntry::make('title'),
--- app/Filament/Resources/SectionResource/Schemas/SectionInfolist.php
+++ app/Filament/Resources/SectionResource/Schemas/SectionInfolist.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace Modules\Cms\Filament\Resources\SectionResource\Schemas;
 
 use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Components\ViewEntry;
 use Filament\Schemas\Components\Component;
+use Filament\Schemas\Components\Section;
+use Modules\Cms\Models\Section as SectionModel;
 use Modules\Xot\Filament\Resources\Schemas\XotBaseResourceInfolist;
 
 class SectionInfolist extends XotBaseResourceInfolist
@@ -13,12 +16,18 @@ class SectionInfolist extends XotBaseResourceInfolist
     /**
      * @return array<string, Component>
      */
-    public static function getInfolistSchema(): array
+    public function getInfolistSchema(): array
     {
         return [
             'name' => TextEntry::make('name'),
             'slug' => TextEntry::make('slug'),
             'blocks' => TextEntry::make('blocks')->markdown(),
+            'preview' => Section::make('Anteprima')->schema([
+                'preview' => ViewEntry::make('preview')
+                    ->view('cms::sections.preview', static fn (SectionModel $record): array => [
+                        'section' => $record,
+                    ]),
+            ]),
         ];
     }
 }
