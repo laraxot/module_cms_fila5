1c9d84182fe4679c84a016926655cb97e5d94ad3
marco76tv <marco76tv@users.noreply.github.com>
2026-09-11 17:09:42 +0000

    Check & fix styling

--- tests/Feature/Auth/RegisterTest.php
+++ tests/Feature/Auth/RegisterTest.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 describe('Register Page', function () {
-    test('register page placeholder', function () {})->todo('Requires public theme components');
-    test('register page placeholder #2', function () {})->todo('Requires public theme components');
+    test('register page placeholder', function () {
+    })->todo('Requires public theme components');
+    test('register page placeholder #2', function () {
+    })->todo('Requires public theme components');
 });
