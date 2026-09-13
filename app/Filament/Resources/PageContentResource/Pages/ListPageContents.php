ccb39062c3963a8a1fcf82e02ac7428ae11674d4
marco76tv <marco76tv@users.noreply.github.com>
2026-09-13 13:27:09 +0000

    Check & fix styling

--- app/Filament/Resources/PageContentResource/Pages/ListPageContents.php
+++ app/Filament/Resources/PageContentResource/Pages/ListPageContents.php
@@ -4,14 +4,12 @@ declare(strict_types=1);
 
 namespace Modules\Cms\Filament\Resources\PageContentResource\Pages;
 
-use Filament\Tables\Columns\TextColumn;
 use Modules\Lang\Filament\Resources\Pages\LangBaseListRecords;
 
 class ListPageContents extends LangBaseListRecords
 {
     // use ListRecords\Concerns\Translatable;
     // public static string $resource = PageContentResource::class;
-    
 
     /*
      * protected function getHeaderActions(): array
