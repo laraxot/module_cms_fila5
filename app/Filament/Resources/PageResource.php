6936dadcdecc3bb4155b550701ce04428db8ac95
marco76tv <marco76tv@users.noreply.github.com>
2026-09-13 13:27:09 +0000

    Check & fix styling

--- app/Filament/Resources/PageResource.php
+++ app/Filament/Resources/PageResource.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Modules\Cms\Filament\Resources;
 
-use Modules\Lang\Filament\Resources\LangBaseResource;
 use Modules\Cms\Models\Page;
+use Modules\Lang\Filament\Resources\LangBaseResource;
 
 class PageResource extends LangBaseResource
 {
