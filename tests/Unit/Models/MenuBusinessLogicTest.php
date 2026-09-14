a37a8526939208292ac2437190ca2a2f245cb12c
marco76tv <marco76tv@users.noreply.github.com>
2026-09-14 15:19:34 +0000

    Check & fix styling

--- tests/Unit/Models/MenuBusinessLogicTest.php
+++ tests/Unit/Models/MenuBusinessLogicTest.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace Modules\Cms\Tests\Unit\Models;
 
 use Modules\Cms\Models\Menu;
-use function Safe\class_uses;
 use Modules\Cms\Tests\TestCase;
 use Modules\Tenant\Models\Traits\SushiToJsons;
 use Modules\Xot\Contracts\HasRecursiveRelationshipsContract;
 use PHPUnit\Framework\Assert;
+
+use function Safe\class_uses;
+
 use Staudenmeir\LaravelAdjacencyList\Eloquent\Builder;
 use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
 
