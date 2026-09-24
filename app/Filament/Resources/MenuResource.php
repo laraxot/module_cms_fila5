090118f5cc157deef8a80a4c1c5321f64d2bafd6
Marco Xot <marco.sottana@gmail.com>
2026-09-24 10:46:20 +0000

    refactor(Cms): change getFormSchema() and getInfolistSchema() methods to non-static in resource classes to comply with final method constraints
    # Conflicts:
    #	app/Filament/Resources/MenuResource.php

--- app/Filament/Resources/MenuResource.php
+++ app/Filament/Resources/MenuResource.php
@@ -10,4 +10,32 @@ use Modules\Xot\Filament\Resources\XotBaseResource;
 class MenuResource extends XotBaseResource
 {
     protected static ?string $model = Menu::class;
+
+    /**
+     * @return array<int, Component>
+     */
+    public function getFormSchemaOld(): array
+    {
+        return [
+            TextInput::make('title')->required()->maxLength(2048),
+            Repeater::make('items')
+                ->schema([
+                    Grid::make(2)->schema([
+                        TextInput::make('title')->required()->columnSpan(1),
+                        TextInput::make('url')
+                            ->required()
+                            ->columnSpan(1),
+                    ]),
+                ]),
+            Radio::make('target')
+                ->options([
+                    '_self' => 'Stessa pagina',
+                    '_blank' => 'Nuova finestra',
+                ])
+                ->default('_self'),
+            SpatieMediaLibraryFileUpload::make('icon')
+                ->collection('cms-icons')
+                ->helperText('Carica un\'icona per il menu'),
+        ];
+    }
 }
