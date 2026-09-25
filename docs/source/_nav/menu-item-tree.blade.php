31c6ce6a77b9c49c1020f8a6baabaff03f1b4499
Marco Xot <marco.sottana@gmail.com>
2026-09-25 12:13:43 +0000

    Remove unused Blade template files related to theme components from the documentation layout.

--- docs/source/_nav/menu-item-tree.blade.php
+++
@@ -1,7 +0,0 @@
----
-module: theme
-topic: menu-item-tree.blade
-canonical: ../../../../../Themes/docs/shared-components/menu-item-tree.blade.php.stub
----
-
-See canonical documentation: ../../../../../Themes/docs/shared-components/menu-item-tree.blade.php.stub
--- docs/source/_nav/menu-tree.blade.php
+++
@@ -1,7 +0,0 @@
----
-module: theme
-topic: menu-tree.blade
-canonical: ../../../../../Themes/docs/shared-components/menu-tree.blade.php.stub
----
-
-See canonical documentation: ../../../../../Themes/docs/shared-components/menu-tree.blade.php.stub
